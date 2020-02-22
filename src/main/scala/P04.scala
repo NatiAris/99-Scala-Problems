b795de7c2bae2a8daaa24d0ce56c7cdbaa3943a9
NatiAris <NatiAris@users.noreply.github.com>
2020-02-22 17:20:01 +0300

    Add problem 4 solution and tests

--- src/main/scala/P04.scala
+++ src/main/scala/P04.scala
@@ -1,7 +1,17 @@
 package org.p99.scala
 
+import scala.annotation.tailrec
+
 object P04 {
 
-  // TODO
+  def length[A](l: List[A]): Int = {
+    @tailrec
+    def go(remaining: List[A], acc: Int): Int = remaining match {
+      case Nil => acc
+      case _ :: t => go(t, acc + 1)
+    }
+
+    go(l, 0)
+  }
 
 }
--- src/test/scala/P04Spec.scala
+++ src/test/scala/P04Spec.scala
@@ -4,7 +4,19 @@ import org.scalatest._
 
 class P04Spec extends UnitSpec {
 
-  // TODO
+  import P04._
+
+  "length" should "be zero for an empty list" in {
+    assert(length(Nil) == 0)
+  }
+
+  "length" should "be one for a single-element list" in {
+    assert(length(1 :: Nil) == 1)
+  }
+
+  "length" should "work correctly for a six-element list" in {
+    assert(length(List(1, 1, 2, 3, 5, 8)) == 6)
+  }
 
 }
 
