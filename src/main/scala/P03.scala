baeab67d2f78386153f7f8429bb5a215c1f1c063
NatiAris <NatiAris@users.noreply.github.com>
2020-02-22 17:19:56 +0300

    Add problem 3 solution and tests

--- src/main/scala/P03.scala
+++ src/main/scala/P03.scala
@@ -1,7 +1,14 @@
 package org.p99.scala
 
+import scala.annotation.tailrec
+
 object P03 {
 
-  // TODO
+  @tailrec
+  def nth[A](n: Int, l: List[A]): A = n match {
+    case n if n < 0 => throw new IllegalArgumentException
+    case 0 => l.headOption.getOrElse { throw new IndexOutOfBoundsException }
+    case n => nth(n - 1, l.tail)
+  }
 
 }
--- src/test/scala/P03Spec.scala
+++ src/test/scala/P03Spec.scala
@@ -4,7 +4,29 @@ import org.scalatest._
 
 class P03Spec extends UnitSpec {
 
-  // TODO
+  import P03._
+
+  val exampleList = List(1, 1, 2, 3, 5, 8)
+
+  "nth" should "return 2 for List(1, 1, 2, 3, 5, 8)" in {
+    val n = 2
+    assert(nth(n, exampleList) == exampleList(n))
+  }
+
+  "nth" should "return first element correctly" in {
+    assert(nth(0, exampleList) == exampleList.head)
+  }
+
+  "nth" should "return last element correctly" in {
+    assert(nth(exampleList.length - 1, exampleList) == exampleList.last)
+  }
+
+  "nth" should "throw IndexOutOfBoundsException" in {
+    assertThrows[IndexOutOfBoundsException](nth(exampleList.length, exampleList))
+  }
+
+  it should "throw IllegalArgumentException for negative n" in {
+    assertThrows[IllegalArgumentException](nth(-1, exampleList))
+  }
 
 }
-
