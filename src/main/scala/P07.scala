90236e4ac4cdc2ea0e013e48f91c92f32b4541b2
NatiAris <NatiAris@users.noreply.github.com>
2020-02-22 17:20:22 +0300

    Add problem 7 solution and tests

--- src/main/scala/P07.scala
+++ src/main/scala/P07.scala
@@ -2,6 +2,25 @@ package org.p99.scala
 
 object P07 {
 
-  // TODO
+  def flatten(lists: List[Any]): List[Any] = {
+    @scala.annotation.tailrec
+    def go(remaining: List[Any], acc: List[Any]): List[Any] = remaining match {
+      case Nil => acc.reverse
+      case Nil :: t => go(t, acc)
+      case (h :: t) :: tt => go(h :: t :: tt, acc)
+      case h :: t => go(t, h :: acc)
+    }
+
+    go(lists, Nil)
+  }
+
+  /*
+   * Suggested solution
+   */
+  //  def flatten(ls: List[Any]): List[Any] = ls flatMap {
+  //    case ms: List[_] => flatten(ms)
+  //    case e => List(e)
+  //  }
+  //
 
 }
--- src/test/scala/P07Spec.scala
+++ src/test/scala/P07Spec.scala
@@ -4,7 +4,18 @@ import org.scalatest._
 
 class P07Spec extends UnitSpec {
 
-  // TODO
+  import P07._
+
+  "flatten" should "work for the example list" in {
+    assertResult(flatten(List(List(1, 1), 2, List(3, List(5, 8)))))(List(1, 1, 2, 3, 5, 8))
+  }
+
+  it should "return Nil if passed Nil" in {
+    assertResult(flatten(Nil))(Nil)
+  }
+
+  it should "return the list itself if the list is flat" in {
+    assertResult(flatten(List(1, 2, 3, 4)))(List(1, 2, 3, 4))
+  }
 
 }
-
