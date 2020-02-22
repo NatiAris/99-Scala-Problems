9b4c0e39b7c22fa7f8c03bc4f748c3e7c15e6690
NatiAris <NatiAris@users.noreply.github.com>
2020-02-22 17:19:34 +0300

    Add problem 1 solution and tests

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -1,7 +1,14 @@
 package org.p99.scala
 
+import scala.annotation.tailrec
+
 object P01 {
 
-  // TODO
+  @tailrec
+  def last[A](l: List[A]): A = l match {
+    case Nil => throw new NoSuchElementException
+    case h :: Nil => h
+    case _ :: t => last(t)
+  }
 
 }
--- src/test/scala/P01Spec.scala
+++ src/test/scala/P01Spec.scala
@@ -4,7 +4,18 @@ import org.scalatest._
 
 class P01Spec extends UnitSpec {
 
-  // TODO
+  import P01._
+
+  "last(List(1, 1, 2, 3, 5, 8))" should "return 8" in {
+    assert(last(List(1, 1, 2, 3, 5, 8)) == 8)
+  }
+
+  "last(List(\"test\"))" should "return \"test\"" in {
+    assert(last(List("test")) == "test")
+  }
+
+  "last(Nil)" should "throw NoSuchElementException" in {
+    assertThrows[NoSuchElementException](last(Nil))
+  }
 
 }
-
