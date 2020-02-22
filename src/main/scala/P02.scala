0f8faf11aef1953b259fa4301167f839bd218683
NatiAris <NatiAris@users.noreply.github.com>
2020-02-22 17:19:46 +0300

    Add problem 2 solution and tests

--- src/main/scala/P02.scala
+++ src/main/scala/P02.scala
@@ -1,7 +1,16 @@
 package org.p99.scala
 
+import java.util.NoSuchElementException
+
+import scala.annotation.tailrec
+
 object P02 {
 
-  // TODO
+  @tailrec
+  def penultimate[A](l: List[A]): A = l match {
+    case Nil | _ :: Nil => throw new NoSuchElementException
+    case x :: _ :: Nil => x
+    case _ :: t => penultimate(t)
+  }
 
 }
--- src/test/scala/P02Spec.scala
+++ src/test/scala/P02Spec.scala
@@ -4,7 +4,23 @@ import org.scalatest._
 
 class P02Spec extends UnitSpec {
 
-  // TODO
+  import P02._
+
+  "penultimate(List(1, 1, 2, 3, 5, 8))" should "return 5" in {
+    assert(penultimate(List(1, 1, 2, 3, 5, 8)) == 5)
+  }
+
+  "penultimate" should "work correctly for a two-element list" in {
+    assert(penultimate(List(1, 2)) == 1)
+  }
+
+  "penultimate" should "throw NoSuchElementException for a single-element list" in {
+    assertThrows[NoSuchElementException](penultimate(List(1)))
+  }
+
+  "penultimate" should "throw NoSuchElementException for an empty list" in {
+    assertThrows[NoSuchElementException](penultimate(Nil))
+  }
 
 }
 
